bac1b0e078979c7394283e3c3566185c879a8227
Juan <a21juanvb@iessanclemente.net>
2021-12-17 22:09:45 +0100

    Modificación parámetro auth

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ def procesar_datos(flow):
         operador = datos[0]
         comando1 = datos[1]
         comando2 = datos[2]
-        pin = datos[3]
+        auth = datos[3]
 
-        resultado = operador, comando1, comando2, pin
+        resultado = operador, comando1, comando2, auth
     return resultado
 
 # Parte que procesa a recuperación dos datos
@@ -23,12 +23,14 @@ if __name__ == '__main__':
     #o arquivo de control está nun repositorio de github
     secuencia = requests.get('https://github.com/a21juanvb/automatizacionTareas/blob/master/flow.txt')
     datos = procesar_datos(secuencia.text)
-    # recortamos os datos para obter as distintas cadeas de instrucións e clave
-    clave = datos[3].split(':')[1]
+    # recortamos os datos para obter as distintas cadeas de instrucións e autorización
+    orde = datos[3].split(':')[1]
     isBackup = datos[1].split(':')[1]
 
-    if clave != '12345':
-        print('Non se pode procesar a orde, dado que a clave non concorda coa anotación de control')
+    print("autorización: ", orde)
+
+    if int(orde) != 1:
+        print('Non se pode procesar a orde, dado que non está autorizada a copia')
     else:
         print("backup: ", isBackup)
         if int(isBackup) == 1:
